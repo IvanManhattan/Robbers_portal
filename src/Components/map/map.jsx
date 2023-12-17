47a97b233eab957b5f7dd9594ad9f190ea6669b3
Vladislav <vladbeskosty7@gmail.com>
2023-12-17 18:31:37 +0300

    Added translation of markers

--- src/Components/map/map.jsx
+++ src/Components/map/map.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import './mapstyle.css'
 
+import './mapstyle.css'
+import {useTranslation} from "react-i18next";
 
 function Map({zoom, cLat, cLng, mLat1, mLng1, mTitle1, mLat2, mLng2, mTitle2, mLat3, mLng3, mTitle3 }) {
+	const { t, i18n } = useTranslation();
 	const { isLoaded } = useJsApiLoader({
 		id: 'google-map-script',
 		googleMapsApiKey: "AIzaSyB9z1kjnqM81t1kYvvxeoQ5P3GR_C3PBJM"
@@ -28,16 +30,16 @@ function Map({zoom, cLat, cLng, mLat1, mLng1, mTitle1, mLat2, mLng2, mTitle2, mL
 		>
 			<Marker
 
-				label={mTitle1}
+				label={t(mTitle1)}
 				position={{ lat: mLat1, lng: mLng1}}
 
 			/>
 			<Marker
-				label={mTitle2}
+				label={t(mTitle2)}
 				position={{lat: mLat2,lng: mLng2}}
 			/>
 			<Marker
-				label={mTitle3}
+				label={t(mTitle3)}
 				position={{lat: mLat3,lng: mLng3}}
 			/>
 		</GoogleMap>
